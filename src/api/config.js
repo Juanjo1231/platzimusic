5d93b20387b76aed8b60596d3c89f3afbd6c2b54
Juanjo Rivera <juanjoriverar@gmail.com>
2018-01-22 20:38:13 -0600

    Initial commit

---
+++ src/api/config.js
@@ -0,0 +1,6 @@
+export default {
+	appName: 'platzimusicjuanjo',
+	apiKey: '843dc7f706bd0f2f1c44dcc83a306aba',
+	secret: '9f4f0af40e30f18fc20127b1b3ae4f29',
+	registeredTo: 'JuanjoVlado'
+}
---
+++ src/components/Artist.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+  
+  li.artist
+    h2: a(v-bind:href="artist.url" target="_blank") {{ artist.name }}
+    img(:key="artist.mbid" v-bind:src="artist.image[2]['#text']")
+
+</template>
+
+<script>
+
+export default {
+  name: 'artist',
+  props: ['artist']
+}
+</script>
+
+<style lang="stylus" scoped>
+  
+  li.artist
+    display block
+    margin 10px 0
+
+</style>
